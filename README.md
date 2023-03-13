2e07b1b45d1a4daa4744fc58354c72ef95e7af77
David Marx <david.marx84@gmail.com>
2023-03-12 19:48:46 -0700

    poc

---
+++ README.md
@@ -0,0 +1,8 @@
+#
+
+## Setup
+
+1. `git clone <this repo>; cd <this repo>`
+2. `pip install -r requirements.txt`
+3. Create a file named `.env` containing one line: `OPENAI_API_KEY=...`, replacing `...` with your key.
+4. `python main.py "becca, play kexp"`
---
+++ main.py
@@ -0,0 +1,52 @@
+from dotenv import load_dotenv
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+import json
+
+load_dotenv()
+
+INTENTS = [
+    "play_internet_radio", 
+    "play_song_by_artist",
+    "get_weather",
+    "current_time",
+    "set_timer",
+    "remind_me",
+]
+
+def build_intent_classifier(intents=INTENTS):
+    template = (
+        "Act as the intent classification component of a home assistant, similar to Amazon Alexa "
+        "(except your name is 'Becca', not 'Alexa').\n"
+        f"Common intents include: {', '.join(intents)}, ...\n"
+        'You receive input in json format: `{{"input":...}}`\n'
+        'You respond in json format: `{{"intent":..., "arguments":{{ ... }}, }}}}`\n\n'
+        '{{"input":{spoken_request}}}'
+    )
+
+    llm = OpenAI(temperature=0.1)
+    prompt = PromptTemplate(
+        input_variables=["spoken_request"],
+        template=template,
+    )
+    return LLMChain(llm=llm, prompt=prompt)
+
+def evaluate(chain, text):
+    response = chain.run(text)
+    return json.loads(response.strip())
+
+if __name__ == '__main__':
+    import sys
+
+    # e2e test
+    chain = build_intent_classifier()
+    if sys.argv[1]:
+        text = sys.argv[1]
+        response = evaluate(chain, text)
+        print(response)
+    else:
+        text = "becca play kexp"
+        response = evaluate(chain, text)
+        print(response)
+        assert response == {'intent':'play_internet_radio', 'arguments':{'station':'KEXP'}}
